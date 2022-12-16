075d716042d0a776e905b78c78e5c25116743e3c
samhereis <samagan888@gmail.com>
2022-12-16 19:21:48 +0600

    init

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public UsersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _dataContext.AppUsers.ToListAsync();
+
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await _dataContext.AppUsers.FindAsync(id);
+
+            return user;
+        }
+    }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        
+    }
+}
