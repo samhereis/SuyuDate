0f7550586d3522452339ada0b2d23b68f42ed6a9
samhereis <samagan888@gmail.com>
2022-12-17 02:29:02 +0600

    password fields added

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -7,6 +7,7 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string UserName { get; set; } = string.Empty;
-        
+        public byte[]? PasswordHash { get; set; }
+        public byte[]? PasswordSalt { get; set; }
     }
 }
